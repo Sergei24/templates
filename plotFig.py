33cebe0754b52dbdb7a97b5ef2b0ff676012472e
Sergei <sergeikozlov24@yandex.ru>
2017-08-29 21:02:24 +0300

    Useful functions for plotting

---
+++ plotFig.py
@@ -0,0 +1,23 @@
+def figsize(scale):
+    '''
+    Return the size of the figures in inches with respect to the test width of latex file
+    '''
+    fig_width_pt = 246.0                          # Get this from LaTeX using \the\textwidth
+    inches_per_pt = 1.0/72.27                       # Convert pt to inch
+    golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
+    fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
+    fig_height = fig_width*golden_mean              # height in inches
+    fig_size = [fig_width,fig_height]
+    return fig_size
+    
+def newfig(width):
+    '''
+    Creating the figure and axes with appropriate size
+    '''
+    plt.clf()
+    fig = plt.figure(figsize=figsize(width))
+    ax = fig.add_subplot(111)
+    return fig, ax
+
+def savefig(filename):
+    plt.savefig('{}.pdf'.format(filename), bbox_inches='tight')
