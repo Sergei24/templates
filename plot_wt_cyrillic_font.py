14bb336c0e9bef503317a394c6f6d0f63a39773b
Sergei <sergeikozlov24@yandex.ru>
2018-06-17 14:24:22 +0300

    Update plot_wt_cyrillic_font.py

--- plot_wt_cyrillic_font.py
+++ plot_wt_cyrillic_font.py
@@ -2,7 +2,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 
-path = '/home/username/fantasy.ttf' \\ way to downloaded new font (especially for cyrillic fonts)
+path = '/home/username/fantasy.ttf' # way to downloaded new font (especially for cyrillic fonts)
 prop = font_manager.FontProperties(fname=path)
 mpl.rcParams['font.family'] = prop.get_name()
 
